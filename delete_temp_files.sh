65d317edeb7e878e74f147d7a40925561417ebc5
Robin Cura <robin.cura@gmail.com>
2018-06-15 17:19:08 +0200

    Ajout script nettoyage fichiers annexes latex
    Former-commit-id: 225fe3fa131d3a53da45a66a2abe89002ab2ca46

---
+++ delete_temp_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+find . \( \
+	-name "*.aux" -o  \
+	-name "*.bbl" -o  \
+	-name "*.blg" -o  \
+	-name "*.maf" -o  \
+	-name "*.log" -o \
+	-name "*.synctex.gz" -o \
+	-name "*.mtc*" -o \
+	-name "*.run.xml" -o \
+	-name "*.out" -o \
+	-name "*.toc" -o \
+	-name "*-blx.bib" \
+	\) -type f -delete
