a7a44f81347c21e39efc8b1728252a2bfbf54ee3
Robin Cura <robin.cura@gmail.com>
2019-11-01 19:05:15 +0100

    clean
    Former-commit-id: cbbd90ea826222bba3e08fc1d0f3299b9045ad52

--- creer_docx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pandoc -s These_Robin_Master.tex \
---latex-engine=xelatex \
---bibliography=chap6/biblio_chap6.bib \
--o chapitre6.docx
--- show_diff.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git latexdiff -o diff_these.pdf --latexmk --latexopt "-xelatex" HEAD~2 --main These_Robin_Master.tex
